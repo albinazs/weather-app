1c9d894173aa7225bf13bc98924f58ab3d82cab7
Albina Starykova <albina.starikova@gmail.com>
2022-10-02 20:20:19 +0300

    Add error handling

--- src/api.js
+++ src/api.js
@@ -6,26 +6,35 @@ const API_KEY = "54548de06c18d589b6cb4816609e68c3";
 export async function getLocation(e) {
   if (search.value == "") return;
   e.preventDefault();
-  const searchRequest = search.value;
-  const request = `http://api.openweathermap.org/geo/1.0/direct?q=${searchRequest}&limit=5&appid=${API_KEY}`;
-  const response = await fetch(request, {
-    mode: "cors",
-  });
-  const result = await response.json();
+  try {
+    const searchRequest = search.value;
+    const request = `http://api.openweathermap.org/geo/1.0/direct?q=${searchRequest}&limit=5&appid=${API_KEY}`;
+    const response = await fetch(request, {
+      mode: "cors",
+    });
+    if (!response.ok) throw new Error(`City ${searchRequest} not found`);
+    const result = await response.json();
 
-  const lat = result[0].lat;
-  const lon = result[0].lon;
-  getWeather(lat, lon);
+    const lat = result[0].lat;
+    const lon = result[0].lon;
+    getWeather(lat, lon);
 
-  search.value = "";
+    search.value = "";
+  } catch (error) {
+    alert(error);
+  }
 }
 
 async function getWeather(lat, lon) {
   const request = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(request, {
-    mode: "cors",
-  });
-  const weatherData = await response.json();
+  try {
+    const response = await fetch(request, {
+      mode: "cors",
+    });
+    const weatherData = await response.json();
 
-  renderWeather(weatherData);
+    renderWeather(weatherData);
+  } catch (error) {
+    alert(error);
+  }
 }
